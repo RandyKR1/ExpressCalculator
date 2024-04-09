f20380080468b6b1b2ff9b1569e16ac70a3e875c
Randall Reichenberger <randall.reichenberger1@gmail.com>
2024-04-09 17:31:01 -0500

    express error notes

--- expressError.js
+++ expressError.js
@@ -1,9 +1,9 @@
-class ExpressError extends Error {
+class ExpressError extends Error { // Error is a built in class, allowing us to create a custom error class
     constructor(message, status) {
-      super();
-      this.message = message;
-      this.status = status;
-      console.error(this.stack);
+        super(); // calls the constructor of the parent class, which in this case is the Error class. 
+        this.message = message; // assigns the provided message parameter to the message property 
+        this.status = status; // assigns the provided status parameter to the status property
+        console.error(this.stack); // 'stack' contains a string representing the stack trace at the time the error was created. It includes information about the sequence of function calls that led to the error.
     }
   }
   
